2de8a117ec0320f0aabec56e212767c9c3b61faa
Melany Baldeon <tochio12910@gmail.com>
2022-11-26 14:12:04 -0500

    added html to show the current weather and function to pull from the api

--- assets/script.js
+++ assets/script.js
@@ -47,3 +47,48 @@ var loadSearchHistory = function() {
     }
 };
 
+var currentWeatherSection = function(cityName) {
+    // get and use data from open weather current weather api end point
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+    // get response and turn it into objects
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        // get city's longitude and latitude
+        var cityLon = response.coord.lon;
+        var cityLat = response.coord.lat;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+            // get response from one call api and turn it into objects
+            .then(function(response) {
+                return response.json();
+            })
+            // get data from response and apply them to the current weather section title 
+            .then(function(response) {
+                searchHistoryList(cityName);
+
+                //add current weather container with border to page 
+                var currentWeatherContainer = $("#current-weather-container");
+                currentWeatherContainer.addClass("current-weather-container");
+
+
+            })
+    })
+};
+
+// called when the search form is submitted 
+$("#search-form").on("submit", function() {
+    event.preventDefault();
+
+    // get name of city searched 
+    var cityName = $("#search-input").val();
+
+    if (cityName === "" || cityName === null) {
+        // alert if search input is empty when submitted
+        alert("Please enter N=name of city.");
+        event.preventDefault();
+    } else {
+        currentWeatherSection(cityName);
+    }
+});
